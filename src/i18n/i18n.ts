e4ce1dfff35dc4c89eb0f8fcff652f845027d9d0
Mirasaki <pcr.hillebrand1998@gmail.com>
2023-11-10 22:21:08 +0100

    fix: explicit only initialize Lang once

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -8,8 +8,13 @@ export const dutchLibrary = nlLib;
 
 export const defaultNS = 'lib';
 
-export const init = (debugEnabled = false) =>
+export const isInitialized = () => Lang.isInitialized;
+
+export const init = (debugEnabled = false) => {
+  if (isInitialized()) return;
   Lang.init({
+    // We don't specify lng, as language is determined
+    // on Discord\'s side by the user\'s locale.
     debug: debugEnabled,
     fallbackLng: 'en',
     defaultNS,
@@ -22,5 +27,7 @@ export const init = (debugEnabled = false) =>
       },
     },
   });
+};
+
 
 export default Lang;
