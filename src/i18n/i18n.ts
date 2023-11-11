ebf7553d8807ca9f1ba5a0d2097813b32e14f8be
Mirasaki <pcr.hillebrand1998@gmail.com>
2023-11-11 17:17:15 +0100

    refactor: clean-up Language implementation

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,17 +1,20 @@
 import Lang from 'i18next';
+import { LocaleString } from 'discord.js';
 
-import enLib from '../../locales/en/lib.json';
-import nlLib from '../../locales/nl/lib.json';
+import enLib from '../../locales/en.json';
+import nlLib from '../../locales/nl.json';
+
+export const locales: LocaleString[] = ['en-GB', 'nl'];
 
 export const englishLibrary = enLib;
 export const dutchLibrary = nlLib;
 
 export const defaultNS = 'lib';
 
-export const isInitialized = () => Lang.isInitialized;
+export const localizationIsInitialized = () => Lang.isInitialized;
 
-export const init = (debugEnabled = false) => {
-  if (isInitialized()) return;
+export const initializeLocalization = (debugEnabled = false) => {
+  if (localizationIsInitialized()) return;
   Lang.init({
     // We don't specify lng, as language is determined
     // on Discord\'s side by the user\'s locale.
@@ -28,6 +31,5 @@ export const init = (debugEnabled = false) => {
     },
   });
 };
-
 
 export default Lang;
--- src/i18n/locales.ts
+++
@@ -1,3 +0,0 @@
-import { LocaleString } from 'discord.js';
-
-export const locales: LocaleString[] = ['en-GB', 'nl'];
